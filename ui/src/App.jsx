fa14991fc930c924c6433696d9ad02551b8de9e1
Mohamed Ayari <ayari.mohamed@web.de>
2025-07-24 23:13:23 +0200

    Adjust home

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -5,7 +5,12 @@ import Home from './pages/home/home.jsx';
 function App() {
   return (
     <BrowserRouter>
-      <Home appName={'URL Shortner'} showRegisterBtn={true} />z
+      <Home appName={'URL Shortner'} showRegisterBtn={true}>
+        <h1>Welcome to the URL Shortner App</h1>
+        <p>Shorten your URLs easily and quickly!</p>
+        <p>Explore the features and enjoy the simplicity of URL shortening.</p>
+        <p>Get started by registering or logging in.</p>  
+      </Home>
     </BrowserRouter>
   );
 }
