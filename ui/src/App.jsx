ffbe36226ec1b9d72bc53158044e815583846f5d
Mohamed Ayari <ayari.mohamed@web.de>
2025-07-30 23:13:04 +0200

    Cleaning the app

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,35 +1,35 @@
-import { Grid, Typography } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/home.jsx';
 import Page from './pages/page/page.jsx';
-import { Container } from '@mui/material';
 
 const Row = ({ children }) => (
-  <Container            /* gives you centered, responsive width */
-    maxWidth="lg"       
-    disableGutters      /* no extra side padding since you’re inside Page */
-    sx={{ px: { xs: 2, sm: 3 } }}  /* optional interior breathing room */
+  <Container /* gives you centered, responsive width */
+    maxWidth='lg'
+    disableGutters /* no extra side padding since you’re inside Page */
+    sx={{ px: { xs: 2, sm: 3 } }} /* optional interior breathing room */
   >
     {children}
   </Container>
 );
 
 function App() {
+  console.log(import.meta.env.VITE_FIREBASE_API_KEY);
   return (
     <BrowserRouter>
-        <Home appName={'URL Shortner'}>
-          <Page>
-            <Row>
-              <Typography variant='h3' gutterBottom>
-                GDG Url Shortner
-              </Typography>
-              <Typography variant='body1' gutterBottom>
-                Google Developer Group
-              </Typography>
-            </Row>
-          </Page>
-        </Home>
+      <Home appName={'URL Shortner'}>
+        <Page>
+          <Row>
+            <Typography variant='h3' gutterBottom>
+              GDG Url Shortner
+            </Typography>
+            <Typography variant='body1' gutterBottom>
+              Google Developer Group
+            </Typography>
+          </Row>
+        </Page>
+      </Home>
     </BrowserRouter>
   );
 }
