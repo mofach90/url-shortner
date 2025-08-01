e84f6c086c62fc69f6631c725eb1ad7a5bb0cffd
Mohamed Ayari <ayari.mohamed@web.de>
2025-08-02 00:06:35 +0200

    Custom google ui prompt

--- ui/src/lib/firebase.js
+++ ui/src/lib/firebase.js
@@ -13,4 +13,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
 export { app, auth, googleProvider };
