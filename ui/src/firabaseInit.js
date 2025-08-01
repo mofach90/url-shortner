0967c8ae707c22eaef80b46efd8692083c73f5ff
Mohamed Ayari <ayari.mohamed@web.de>
2025-08-02 01:41:42 +0200

    add listner

---
+++ ui/src/firabaseInit.js
@@ -0,0 +1,14 @@
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './lib/firebase.js';
+import useAuthStore from './store/authStore.js';
+
+const set = useAuthStore.getState().setState; 
+
+onAuthStateChanged(auth, async (firebaseUser) => {
+  if (firebaseUser) {
+    const idToken = await firebaseUser.getIdToken();
+    set({ user: firebaseUser, idToken, loading: false });
+  } else {
+    set({ user: null, idToken: null, loading: false });
+  }
+});
