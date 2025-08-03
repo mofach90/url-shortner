49806739e779e1e801969af9fe80ba92e54feffe
Mohamed Ayari <ayari.mohamed@web.de>
2025-08-03 23:05:59 +0200

    Add the router

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,11 +1,17 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LandingPage from './pages/LandingPage.jsx';
 
 function App() {
   return (
     <BrowserRouter>
-      <LandingPage />
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route
+          path='/dashboard'
+          element={<ProtectedRoute element={<DashboardPage />} />}
+        />
+      </Routes>
     </BrowserRouter>
   );
 }
