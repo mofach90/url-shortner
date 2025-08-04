e0af1576fed9c99ab46f043e299e70b6f3fba243
Mohamed Ayari <ayari.mohamed@web.de>
2025-08-04 15:05:57 +0200

    wop

--- ui/src/pages/dashboard/dashboard.jsx
+++ ui/src/pages/dashboard/dashboard.jsx
@@ -1,3 +1,3 @@
-export default function LandingPage() {
+export default function DashboardPage() {
   return <h1>Welcome to GDG URL Shortener</h1>;
 }
--- ui/src/routes/ProtectedRoute.jsx
+++ ui/src/routes/ProtectedRoute.jsx
@@ -1,8 +1,12 @@
-import useAuthStore from '../store/authStore.js';
+import { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
+import useAuthStore from '../store/authStore.js';
 
 export default function ProtectedRoute({ element }) {
   const { user, loading } = useAuthStore();
+  useEffect(() => {
+    console.log('ProtectedRoute: checking auth state', { user, loading });
+  }, [loading, user]);
 
   if (loading) return null; // or a spinner
   if (!user) return <Navigate to='/' replace />;
