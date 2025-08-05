c10d6b1b796de7edcc160463b3e26f9531a3a3ee
Mohamed Ayari <ayari.mohamed@web.de>
2025-08-06 00:37:28 +0200

    Add a theme store

--- ui/src/store/themeStore.js
+++ ui/src/store/themeStore.js
@@ -1,3 +1,6 @@
+import { create } from 'zustand';
+
+
 const STORAGE_KEY = 'color-mode';
 
 const getInitialMode = () => {
@@ -12,3 +15,17 @@ const getInitialMode = () => {
   // 3) default to 'light'
   return 'light';
 };
+const useThemeStore = create((set) => ({
+  mode: getInitialMode(),          // 'light' or 'dark'
+  setMode: (m) => {
+    localStorage.setItem(STORAGE_KEY, m);
+    set({ mode: m });
+  },
+  toggleMode: () => set((s) => {
+    const next = s.mode === 'light' ? 'dark' : 'light';
+    localStorage.setItem(STORAGE_KEY, next);
+    return { mode: next };
+  }),
+}));
+
+export default useThemeStore;
