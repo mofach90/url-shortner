799affbc61ca068073d219570c46f92ec2c66918
Mohamed Ayari <ayari.mohamed@web.de>
2025-08-05 22:55:35 +0200

    Work on dashboard

--- ui/src/pages/dashboard/dashboard.jsx
+++ ui/src/pages/dashboard/dashboard.jsx
@@ -1,3 +1,34 @@
-export default function DashboardPage() {
-  return <h1>Welcome to GDG URL Shortener</h1>;
+import { Box } from '@mui/material';
+import Bar from './bar.jsx';
+
+
+const DashboardPage = () => {
+  return (
+    <Box
+      display='flex'
+      flexDirection='column'
+      sx={{ minHeight: '100vh', width: '100%' }}
+    >
+      {/* Header */}
+      <Box component='header' sx={{ width: '100%' }}>
+        <Bar  />
+      </Box>
+
+      {/* Main content: grows to fill the middle */}
+      <Box
+        component='main'
+        sx={{
+          flexGrow: 1,
+          width: '100%',
+          // optional: add responsive padding
+          px: { xs: 2, sm: 3, md: 4 },
+          py: { xs: 2, sm: 3, md: 4 },
+        }}
+      >
+      </Box>
+
+    </Box>
+  );
 }
+
+export default DashboardPage;
