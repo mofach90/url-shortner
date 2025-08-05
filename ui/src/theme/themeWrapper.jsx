c5574d4ff278472cb2e451b5ebf24b0d80b03815
Mohamed Ayari <ayari.mohamed@web.de>
2025-08-06 00:57:03 +0200

    Add the wrapper

--- ui/src/theme/themeWrapper.jsx
+++ ui/src/theme/themeWrapper.jsx
@@ -1,14 +1,14 @@
-// src/theme/ThemeWrapper.jsx
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import { themes }          from './themes';
-import useThemeStore       from '../store/themeStore.js';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import useThemeStore from '../store/themeStore.js';
+import { themes } from './theme.js';
 
-export default function ThemeWrapper({ children }) {
+const ThemeWrapper = ({ children }) => {
   const mode = useThemeStore((s) => s.mode);
   return (
     <ThemeProvider theme={themes[mode]}>
-      <CssBaseline />   {/* resets body background & typography */}
+      <CssBaseline /> {/* resets body background & typography */}
       {children}
     </ThemeProvider>
   );
 }
+export default ThemeWrapper;
