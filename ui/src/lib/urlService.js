f439f0c56de5fb560cb66557fb4c37560708b757
Mohamed Ayari <ayari.mohamed@web.de>
2025-10-24 00:49:55 +0200

    feat: implement createShortUrl function for URL shortening

---
+++ ui/src/lib/urlService.js
@@ -0,0 +1,37 @@
+import useAuthStore from "../store/authStore.js";
+
+
+export async function createShortUrl(longUrl) {
+  const { idToken } = useAuthStore.getState();
+
+  const isLocal =
+    window.location.hostname === "127.0.0.1" ||
+    window.location.hostname === "localhost";
+
+  const functionsEmuUrl = `http://127.0.0.1:5001/mo-url-shortner/us-central1/api/shorten`;
+
+  const endpoint = isLocal ? functionsEmuUrl : `/api/shorten`;
+
+  const res = await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      ...(idToken ? { Authorization: `Bearer ${idToken}` } : {}),
+    },
+    body: JSON.stringify({ longUrl }),
+  });
+
+  
+  let data;
+  try {
+    data = await res.json();
+  } catch {
+    data = null;
+  }
+
+  if (!res.ok) {
+    const msg = data?.error || `Request failed with ${res.status}`;
+    throw new Error(msg);
+  }
+  return data; 
+}
